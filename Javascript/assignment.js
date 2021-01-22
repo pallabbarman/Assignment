447b9b4935fc6a32cf5f7676d670aa390a664425
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-01-22 14:36:14 +0600

    third problem solved

--- Javascript/assignment.js
+++ Javascript/assignment.js
@@ -9,19 +9,44 @@ function kilometerToMeter(value) {
         return "Distance cann't be negative";
     }
 }
-var result = kilometerToMeter(5);
+var result = kilometerToMeter(10);
 console.log(result);
 
 // budget Calculator
 
 function budgetCalculator(watch, mobile, laptop) {
     if (watch >= 0 && mobile >= 0 && laptop >= 0) {
-        var productPrice = (watch * 50) + (mobile * 100) + (laptop * 500);
-        return productPrice;
+        var totalProductCost = (watch * 50) + (mobile * 100) + (laptop * 500);
+        return totalProductCost;
     }
     else {
         return "Price cann't be negative";
     }
 }
-var totalPrice = budgetCalculator(10, 8, -10);
-console.log(totalPrice);
+var numberOfProducts = budgetCalculator(10, -8, 10);
+console.log(numberOfProducts);
+
+// hotel cost 
+
+function hotelCost(days) {
+    if (days > 0) {
+        var rent = 0;
+        for (var i = 1; i <= days; i++) {
+            if (i >= 1 && i <= 10) {
+                rent += 100;
+            }
+            else if (i >= 11 && i <= 20) {
+                rent += 80;
+            }
+            else if (i > 20) {
+                rent += 50;
+            }
+        }
+        return rent;
+    }
+    else {
+        return "Day's cann't be negative or symbol";
+    }
+}
+var totalRent = hotelCost(21);
+console.log(totalRent);
